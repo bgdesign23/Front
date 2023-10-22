1cbf76f8e577cfd4239fa382059494991095ede8
mspano <mspanopablo@gmail.com>
2023-10-22 15:10:41 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+ReactDOM.createRoot(document.getElementById("root"))
+  .render
+  // <BrowserRouter>
+  //   <Provider store={store}>
+  //     <App />
+  //   </Provider>
+  // </BrowserRouter>
+  ();
