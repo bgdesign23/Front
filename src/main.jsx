9462b0eb28eae2844788653724fff6d1b54d4c93
mspano <mspanopablo@gmail.com>
2023-10-25 11:02:18 -0300

    no hice nada

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,10 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById("root"))
-  .render
-  // <BrowserRouter>
-  //   <Provider store={store}>
-  //     <App />
-  //   </Provider>
-  // </BrowserRouter>
-  ();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
+  </BrowserRouter>
+);
