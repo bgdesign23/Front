4fba2a542068d258e331ef4844dd3d88d1d42b49
Mayra Camiccia <mayraacamiccia@gmail.com>
2023-10-26 01:12:20 -0300

    actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import LoginForm from "./views/LoginForm/LoginForm";
 import FormRegistro from "./views/FormRegistro/FormRegistro";
 import NavBar from "./Components/NavBar/NavBar";
 import { useDispatch, useSelector } from "react-redux";
-import { getProductsAction } from "./redux/actions";
+import { getProductsAction } from "./Redux/actions";
 import Detail from "./views/Detail/Detail";
 
 
