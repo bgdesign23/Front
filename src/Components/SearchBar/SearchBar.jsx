c0b702d7810b5bc9c3d29b7581f27afbc739ed75
mspano <mspanopablo@gmail.com>
2023-10-26 21:00:47 -0300

    searchbar funcionando

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,9 +1,11 @@
 import styles from "../SearchBar/SearchBar.module.css";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { getByName, reset_ProductList } from "../../Redux/actions";
 
 const SearchBar = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [searchState, setSearchState] = useState("");
 
@@ -11,6 +13,7 @@ const SearchBar = () => {
     event.preventDefault();
     console.log(searchState);
     dispatch(getByName(searchState));
+    navigate(`/home/product`);
   };
 
   const handle_input = (event) => {
