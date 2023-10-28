c18897fe7751fb6752f9cdb5578fd4e3fe5e6ddb
mspano <mspanopablo@gmail.com>
2023-10-28 12:08:02 -0300

    localStorage

--- src/Components/CartShop/CartShop.jsx
+++ src/Components/CartShop/CartShop.jsx
@@ -1,28 +1,25 @@
-
- /*import React, { useState } from 'react'; */
+import React, { useState } from "react";
 
 function ShoppingCart() {
-  /*const [cart, setCart] = useState([]);
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-  const addToCart = () => {
-  };
-
-  const removeFromCart = () => {
-  };
+  const removeFromCart = () => {};
 
   return (
     <div>
       <h2> ShoppingCart </h2>
-      
-        {cart.map(( ) => (
-          
-    <button onClick={() => removeFromCart( )}> Eliminar </button>
-        
-        ))}
-  
+
+      {cart.map((producto, index) => (
+        <div key={index}>
+          <img src={producto.image} alt="" />
+          <h1>{producto.name}</h1>
+          <p>Precio: $ {producto.price},00 </p>
+
+          <button onClick={() => removeFromCart()}> Eliminar </button>
+        </div>
+      ))}
     </div>
-  ); */
+  );
 }
 
 export default ShoppingCart;
-
