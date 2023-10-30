474a2c8445ad5dd57603bba5f7d19796e4c62d8a
mspano <mspanopablo@gmail.com>
2023-10-29 22:03:56 -0300

    .

--- src/views/Product/Product.jsx
+++ src/views/Product/Product.jsx
@@ -13,32 +13,34 @@ export default function Cards({ productos }) {
   const pagedSection = productos.slice(0, paged);
   return (
     <div className={Styles.contenCard}>
-    <>
-      <Filters />
-      {!productos.length ? (
-        <div>
-          <h1>No se encontraron productos relacionados</h1>
-        </div>
-      ) : (
-        <>
-          <div className={Styles.products_container}>
-            {pagedSection.map((product) => (
-              <Card
-                key={product.id}
-                id={product.id}
-                name={product.name}
-                description={product.description}
-                types={product.type}
-                stock={product.stock}
-                price={product.price}
-                image={product.image}
-                category={product.CategoryId}
-              />
-            ))}
+      <>
+        <Filters />
+        {!productos.length ? (
+          <div>
+            <h1>No se encontraron productos relacionados</h1>
           </div>
-          <button onClick={handlePaged}>Más Productos</button>
-        </>
-      )}
-    </></div>
+        ) : (
+          <>
+            <div className={Styles.products_container}>
+              {pagedSection.map((product) => (
+                <Card
+                  key={product.id}
+                  id={product.id}
+                  name={product.name}
+                  description={product.description}
+                  types={product.type}
+                  stock={product.stock}
+                  price={product.price}
+                  image={product.image}
+                  category={product.CategoryId}
+                  amount={product.amount} //cantidad
+                />
+              ))}
+            </div>
+            <button onClick={handlePaged}>Más Productos</button>
+          </>
+        )}
+      </>
+    </div>
   );
 }
