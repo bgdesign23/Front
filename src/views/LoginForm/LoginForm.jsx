8d221ddfef834855e700d8656b9899e6de1c61b6
BenjaMura <benjaminmuratore1@gmail.com>
2023-11-01 18:39:23 -0300

    Arreglo Cors2

--- src/views/LoginForm/LoginForm.jsx
+++ src/views/LoginForm/LoginForm.jsx
@@ -6,7 +6,8 @@ import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import Swal from 'sweetalert2';
 
-const URL = import.meta.env.VITE_BACK_URL || "http://localhost:3001";
+// const URL = "http://localhost:3001";
+const URL = "https://backend-muebles.vercel.app";
 
 function LoginForm() {
   const dispatch = useDispatch();
