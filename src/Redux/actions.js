eeeaf6abf875d729ca7a5cf1c16c1f40271142d2
mspano <mspanopablo@gmail.com>
2023-11-02 12:22:04 -0300

    cambios css cantidad carrito!

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -28,8 +28,8 @@ import {
   SET_USER,
 } from "../Redux/actionsTypes";
 
-// const URL = "http://localhost:3001";
-const URL = "https://backend-muebles.vercel.app";
+const URL = "http://localhost:3001";
+// const URL = "https://backend-muebles.vercel.app";
 
 export const getProductsAction = () => {
   return async (dispatch) => {
