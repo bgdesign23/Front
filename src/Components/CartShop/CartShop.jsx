7b28f3d0066e1c7cffac9395ad10daeed39b704b
Benjamín Muratore <benjaminmuratore1@gmail.com>
2023-11-05 23:16:10 -0300

    Update CartShop.jsx

--- src/Components/CartShop/CartShop.jsx
+++ src/Components/CartShop/CartShop.jsx
@@ -25,21 +25,20 @@ function validateCoupon(couponCode) {
     new Date(welcomeCoupon.expiration) >= currentDate &&
     welcomeCoupon.usagesAvailable > 0
   ) {
-    return welcomeCoupon.discount; 
+    return welcomeCoupon.discount;
   }
 
-  return 0; 
+  return 0;
 }
+
 function ShoppingCart() {
+  initMercadoPago("TEST-f0c64837-0fc1-441b-85ea-20be004df16e");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [preferenceId, setPreferenceId] = useState(null);
   const [couponCode, setCouponCode] = useState("");
-  const dispatch = useDispatch();
   const [discount, setDiscount] = useState(0);
-  const [totalWithDiscount, setTotalWithDiscount] = useState(0); 
-
-
-  initMercadoPago("TEST-f0c64837-0fc1-441b-85ea-20be004df16e");
-  const navigate = useNavigate();
+  const [totalWithDiscount, setTotalWithDiscount] = useState(0);
   const [cart, setCart] = useState(
     JSON.parse(localStorage.getItem("cart")) || []
   );
@@ -55,28 +54,26 @@ function ShoppingCart() {
   }, 0);
 
   const deleteProduct = (productId) => {
-    // show alert confirmation
     Swal.fire({
-      title: "ojo!",
+      title: "Ojo!",
       text: "Estas sacando un producto de tu orden, estas seguro?",
-      icon: "Atención",
+      icon: "warning",
       showCancelButton: true,
       background: "#1A1A1A",
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#394754",
       confirmButtonText: "Eliminar",
       cancelButtonText: "cancelar",
     }).then((result) => {
       if (result.isConfirmed) {
-        // delete pokemon if is confirmed
         const updatedCart = cart.filter((product) => product.id !== productId);
         setCart(updatedCart);
         localStorage.setItem("cart", JSON.stringify(updatedCart));
         Swal.fire({
           title: "Eliminado",
-          text: "El producto ya no esta en cariito",
+          text: "El producto ya no está en tu carrito",
           background: "#1A1A1A",
-          confirmButtonColor: "#3085d6",
+          confirmButtonColor: "#394754",
         });
       }
     });
@@ -113,124 +110,132 @@ function ShoppingCart() {
   };
 
   const calculateTotalPrice = (product) => {
-    const discountedPrice = product.price * (1 - discount);
-  return product.amount * discountedPrice;
+    const totalPrice = product.price * product.amount;
+    return totalPrice;
   };
 
   function formatthousand(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   }
 
-
-const [localCouponCode, setLocalCouponCode] = useState("");
-
-const handleApplyCoupon = () => {
-  const newDiscount = validateCoupon(couponCode);
-  setLocalCouponCode(couponCode);
-  
-  // Calcula el precio con descuento antes de continuar
-  if (newDiscount > 0) {
-    const totalWithDiscount = numero * (1 - newDiscount);
+  const handleApplyCoupon = async () => {
+    const newDiscount = validateCoupon(couponCode);
     setDiscount(newDiscount);
-    setTotalWithDiscount(totalWithDiscount);
-  } else {
-    setDiscount(0);
-    setTotalWithDiscount(numero);
-  }
-  dispatch(applyCoupon(couponCode));
-  setCouponCode("");
-};
+    dispatch(applyCoupon(couponCode));
+    setCouponCode("");
 
-const handleBuy = async () => {
-  console.log("La función handleBuy se está ejecutando.");
+    if (newDiscount > 0) {
+      const totalWithDiscount = numero * (1 - newDiscount);
+      setTotalWithDiscount(totalWithDiscount);
+    } else {
+      setDiscount(0);
+      setTotalWithDiscount(numero);
+    }
+  };
 
-  try {
-    const id = await dispatch(createPreference(cart));
+  const handleBuy = async () => {
+    const updatedCart = cart.map(producto => {
+      const discountedPrice = producto.price * (1 - discount);
+      return {
+        ...producto,
+        price: discountedPrice,
+      };
+    });
+    try {
+      const id = await dispatch(createPreference(updatedCart));
+      setPreferenceId(id);
+    } catch (error) {
+      console.log("Error al crear la preferencia:", error);
+    }
+  };
 
-    setPreferenceId(id);
-  } catch (error) {
-    console.log("Error al crear la preferencia:", error);
-  }
-};
-
- 
-  
- return (
-  <div className={Styles.all_container}>
-    <div className={Styles.ShoppingCart_container}>
+  return (
+    <div className={Styles.all_container}>
       <button
         className={Styles.backBtn}
         onClick={() => navigate("/home/product")}
       >
         Back
       </button>
-      <div className={Styles.tittle}>
-        <h1>tu carrito de compras</h1>
-      </div>
-      <div>
-        {cart.map((producto, index) => (
-          <CartCard
-            key={producto.id}
-            id={producto.id}
-            name={producto.name}
-            description={producto.description}
-            types={producto.type}
-            stock={producto.stock}
-            price={producto.price}
-            image={producto.image}
-            category={producto.CategoryId}
-            amount={producto.amount}
-            deleteProduct={deleteProduct}
-            handleAmount_Up={handleAmount_Up}
-            handleAmount_Down={handleAmount_Down}
-            totalPriceProduct={calculateTotalPrice(producto)}
-            formatthousand={formatthousand}
-            disableDecreaseButton={producto.amount === 1}
-          />
-        ))}
-      </div>
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          className: "",
-          style: {
-            border: "1px solid #713200",
-            padding: "16px",
-            color: "#191919",
-            background: "#ffff",
-          },
-        }}
-      />
-    </div>
-    <div className={Styles.resumeCart}>
-      <div>
-        <p>Resumen de compra</p>
-      </div>
-      <div>
-        <p>Cantidad productos: {cantidad}</p>
-        <p>Total a pagar: ${formatthousand(numero)}</p> 
-        {discount > 0 && (
-          <p>Total a pagar con descuento: ${formatthousand(totalWithDiscount)}</p>
-        )} 
-      </div>
-      <div className={Styles.cupon_container}>
-        <input
-          type="text"
-          placeholder="Ingresa el código del cupón"
-          value={couponCode}
-          onChange={(e) => setCouponCode(e.target.value)}
+      <div className={Styles.ShoppingCart_container}>
+        <div className={Styles.tittle}>
+          <p>Tu carrito de compras</p>
+        </div>
+        <div>
+          {cart.length === 0 ? (
+            <h1 className={Styles.carVacio}>Tu carrito esta vacío!</h1>
+          ) : (
+            cart.map((producto, index) => (
+              <CartCard
+                key={producto.id}
+                id={producto.id}
+                name={producto.name}
+                description={producto.description}
+                types={producto.type}
+                stock={producto.stock}
+                price={producto.price}
+                image={producto.image}
+                category={producto.CategoryId}
+                amount={producto.amount}
+                deleteProduct={deleteProduct}
+                handleAmount_Up={handleAmount_Up}
+                handleAmount_Down={handleAmount_Down}
+                totalPriceProduct={calculateTotalPrice(producto)}
+                formatthousand={formatthousand}
+                disableDecreaseButton={producto.amount === 1}
+              />
+            ))
+          )}
+        </div>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            className: "",
+            style: {
+              border: "1px solid #713200",
+              padding: "16px",
+              color: "#191919",
+              background: "#ffff",
+            },
+          }}
         />
-        <button className={Styles.btncupon} onClick={handleApplyCoupon}>
-          Aplicar Cupón
-        </button>
       </div>
-      <button className={Styles.btn} onClick={handleBuy}>
-        Continuar con la compra
-      </button>
-      {preferenceId && <Wallet initialization={{ preferenceId }} />}
+      <div className={Styles.resumeCart}>
+        <div className={Styles.titulo}>
+          <p>Resumen de compra</p>
+        </div>
+        <div>
+          <div className={Styles.details}>
+            <div className={Styles.detailsHijo}>
+              <p>Productos: ({cantidad})</p>
+              <div className={Styles.cupon_container}>
+                <input
+                  type="text"
+                  placeholder=" Ingresa el código del cupón"
+                  value={couponCode}
+                  onChange={(e) => setCouponCode(e.target.value)}
+                />
+                <button className={Styles.btncupon} onClick={handleApplyCoupon}>
+                  Aplicar Cupón
+                </button>
+              </div>
+              <p>Total: ${formatthousand(numero)}</p>
+            </div>{" "}
+            {discount > 0 && (
+              <p>
+                Total a pagar con descuento: $
+                {formatthousand(totalWithDiscount)}
+              </p>
+            )}{" "}
+          </div>
+        </div>
+        <button className={Styles.btn} onClick={handleBuy}>
+          CONTINUAR CON LA COMPRA
+        </button>
+        {preferenceId && <Wallet initialization={{ preferenceId }} />}
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default ShoppingCart;
