e5a4c1ab420a577662ea810c237f49fb2833017c
valenpe <valentinaperez230701@gmail.com>
2023-11-16 04:30:21 -0300

    ultimas modificaciones

--- src/views/userPerfil/Favorites/MyFavorites.jsx
+++ src/views/userPerfil/Favorites/MyFavorites.jsx
@@ -1,54 +1,53 @@
-import Menu from "../../userPerfil/Menu/Menu"
-import Card from "../../Product/Card"
-import { getFav, deleteFav } from "../../../Redux/actions"
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-
+import Menu from "../../userPerfil/Menu/Menu";
+import Card from "../../Product/Card";
+import { getFav, deleteFav } from "../../../Redux/actions";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Styles from "../Favorites/MyFavorites.module.css";
 const MyFavs = () => {
-    const dispatch = useDispatch()
-    const favorites = useSelector((state) => state.favorites)
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites);
 
-    useEffect(()=> {
-        dispatch(getFav())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(getFav());
+  }, [dispatch]);
 
-    const handleDeleteFav = (id) => {
-        dispatch(deleteFav(id))
-    }
+  const handleDeleteFav = (id) => {
+    dispatch(deleteFav(id));
+  };
 
-    return (
-        <>
-        <div>
-            <Menu/>
+  return (
+    <div className={Styles.containerAll}>
+      <div className={Styles.boxLeft}>
+        <Menu />
+      </div>
+      <div className={Styles.boxRight}>
+        <div className={Styles.containerFavortite}>
+          {favorites?.map((product) => (
+            <div key={product.id} className={Styles.containerProduct}>
+              <Card
+                id={product.id}
+                name={product.name}
+                description={product.description}
+                types={product.type}
+                stock={product.stock}
+                price={product.price}
+                image={product.image}
+                material={product.material}
+                color={product.color}
+              />
+              <button
+                onClick={() => handleDeleteFav(product.id)}
+                className={Styles.buttonDelete}
+              >
+                Eliminar
+              </button>
+            </div>
+          ))}
         </div>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <h1> Favoritos: </h1>
-         {favorites?.map((product) =>
-         <div key= {product.id}>
-            <Card            
-              id={product.id}
-              name= {product.name}
-              description={product.description}
-              types={product.type}
-              stock={product.stock}
-              price={product.price}
-              image={product.image}
-              material={product.material}
-              color={product.color}                                
-            />            
-            <button onClick={() => handleDeleteFav(product.id)} >Eliminar de Favoritos</button>
-        </div>
-        )}
-                
-        </>
-    )
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default MyFavs
+export default MyFavs;
